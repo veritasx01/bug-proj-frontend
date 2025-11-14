f98bbd2be9d4de3f6ae357ed3cd4c4bcf9070f6e
veritas <liorgby2@gmail.com>
2025-11-14 12:48:56 +0200

    test

--- src/services/bug.service.js
+++ src/services/bug.service.js
@@ -3,7 +3,9 @@ const axios = Axios.create({
   withCredentials: true,
 });
 
-const BASE_URL = 'http://localhost:3030/api/bug/';
+const BASE_URL = (process.env.NODE_ENV !== 'development') ?
+    '/api/bug/' :
+    '//localhost:3030/api/bug/'
 
 export const bugService = {
   query,
